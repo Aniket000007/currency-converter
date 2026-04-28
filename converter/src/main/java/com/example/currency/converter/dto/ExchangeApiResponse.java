b35eb6e2e0bc98c773d17db9135c4b2e7e2b5516
Macbook Air <macbookair@Macbook-Airs-MacBook-Pro.local>
2026-04-28 12:56:47 +0530

    fix

--- converter/src/main/java/com/example/currency/converter/dto/ExchangeApiResponse.java
+++ converter/src/main/java/com/example/currency/converter/dto/ExchangeApiResponse.java
@@ -11,4 +11,8 @@ public class ExchangeApiResponse {
     private String base_code;
 
     private Map<String, Double> conversion_rates;
+
+    public Map<String, Double> getConversion_rates() {
+        return conversion_rates;
+    }
 }
